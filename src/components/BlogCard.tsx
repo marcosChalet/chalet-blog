59cfcffe23df00d6f827b7cb563b1bcfd7cb0bc5
marcosChalet <chaletmarcos@gmail.com>
2023-04-20 20:48:16 -0300

    feat: projeto base

---
+++ src/components/BlogCard.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function BlogCard({
+  title,
+  slug,
+  author,
+  coverPhoto,
+  datePublished,
+}: any) {
+  return (
+    <div className="my-3 sm:mx-3 h-72 min-w-[20rem] max-w-xs flex-shrink-0 flex-col bg-slate-200 rounded-lg shadow-xl overflow-hidden text-slate-800">
+      <Link href={"/posts/" + slug}>
+        <div className="h-42">
+          <Image
+            width={500}
+            height={300}
+            className="w-full h-full bg-cover"
+            src={coverPhoto.url}
+            alt=""
+          />
+        </div>
+      </Link>
+      <div className="px-2 py-4 font-semibold flex flex-col h-32 justify-between">
+        <h2 className="pb-3">{title}</h2>
+        <div className="flex items-center justify-between">
+          <div className="flex items-center">
+            <Image
+              width={100}
+              height={100}
+              className="rounded-full w-8 h-8 mr-2"
+              src={author.avatar.url}
+              alt=""
+            />
+            <h3 className="text-xs">@{author.name}</h3>
+          </div>
+          <div>
+            <h3 className="text-xs">{datePublished}</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/posts/[slug].tsx
@@ -0,0 +1,101 @@
+import { GraphQLClient, gql } from "graphql-request";
+import Image from "next/image";
+
+const graphcms = new GraphQLClient(process.env.GRAPHQL_CLIENT || "");
+
+const QUERY = gql`
+  query Post($slug: String!) {
+    post(where: { slug: $slug }) {
+      id
+      title
+      slug
+      datePublished
+      author {
+        id
+        name
+        avatar {
+          url
+        }
+      }
+      content {
+        html
+      }
+      coverPhoto {
+        id
+        url
+      }
+    }
+  }
+`;
+
+const SLUGLIST = gql`
+  {
+    posts {
+      slug
+    }
+  }
+`;
+
+export async function getStaticPaths() {
+  const { posts }: any = await graphcms.request(SLUGLIST);
+  return {
+    paths: posts.map((post: any) => ({ params: { slug: post.slug } })),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }: any) {
+  try {
+    const slug = params.slug;
+    const data: any = await graphcms.request(QUERY, { slug });
+    const post: any = data.post;
+
+    return {
+      props: {
+        post,
+      },
+      revalidate: 10,
+    };
+  } catch (error) {
+    console.log(error);
+  }
+
+  return {
+    props: {},
+  };
+}
+
+export default function BlogPost({ post }: any) {
+  return (
+    <main className="text-slate-300 text-lg max-w-5xl m-auto">
+      <Image
+        width={1200}
+        height={600}
+        quality={100}
+        priority
+        src={post.coverPhoto.url}
+        alt=""
+        className="w-full h-[25rem] object-cover"
+      />
+      <h2 className="text-4xl pt-5 text-slate-300">{post.title}</h2>
+      <div className="flex justify-start items-center mt-3 mb-14">
+        <Image
+          width={300}
+          height={300}
+          quality={100}
+          priority
+          src={post.author.avatar.url}
+          alt=""
+          className="w-7 h-7 rounded-full border-2 border-sky-500 mr-2"
+        />
+        <div className="flex justify-start items-center italic text-base">
+          <h6 className="mr-2">
+            By <b>{post.author.name}</b>
+          </h6>
+          <h6>{post.datePublished}</h6>
+        </div>
+      </div>
+      <div dangerouslySetInnerHTML={{ __html: post.content.html }}></div>
+    </main>
+  );
+}
